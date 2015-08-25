0febf55de72bfe67a7f3aa89f8e767e968be3895
cannin <cannin@gmail.com>
2015-08-25 16:34:28 -0400

    Added function to read in Zeptosens-formatted data

--- R/readZeptosensExport.R
+++ R/readZeptosensExport.R
@@ -10,7 +10,63 @@
 #' 
 #' @concept zeptosensPkg
 #' @export 
-readZeptosensExport <- function(nrep=NULL, ncond=NULL, ntime=NULL, nab=NULL, outputFile=NULL) {
+readZeptosensExport <- function(inputFile=NULL, outputFile=NULL, sampleNameEntries=NULL) {
+    message("NOTE: Sample Name in *_RFI_Export_Table.xls is assumed to be in the format sampleNumber_sampleName_drug_time_dose_replicateNumber. Use 'samplenNameEntries' parameter to modify as described in documentation.")
+    
+    colClasses <- c("character", "numeric", "numeric", "character", "character", "character", 
+      "numeric", "character", "character", "character", "character", "character", 
+      "numeric", "numeric", "numeric", "character", "numeric", "numeric", 
+      "numeric", "numeric", "numeric", "numeric", "numeric", "character", 
+      "character", "character", "numeric")
+    
+    data <- read.xlsx2(inputFile, sheetIndex=1, colClasses=colClasses, stringsAsFactors=FALSE)
+
+    splitSampleNames <- strsplit(data[, "Sample.Name"], "_")
+    
+    df <- NULL
+    for(i in 1:nrow(data)) {
+        sampleNameNumberIdx <- which(sampleNameEntries == "sampleNameNumber")
+        sampleNameIdx <- which(sampleNameEntries == "sampleName")
+        treatmentIdx <- which(sampleNameEntries == "treatment")
+        timeIdx <- which(sampleNameEntries == "time")
+        replicateIdx <- which(sampleNameEntries == "replicate")
+        
+        tmpDf <- data.frame(
+            sampleNumber=data[i, "Sample."],
+            sampleNameNumber=splitSampleNames[[i]][sampleNameNumberIdx],
+            sampleName=splitSampleNames[[i]][sampleNameIdx],
+            treatment=splitSampleNames[[i]][treatmentIdx],
+            time=splitSampleNames[[i]][timeIdx],
+            replicate=splitSampleNames[[i]][replicateIdx],
+            antibody=data[i, "Analyte"],
+            readout=data[i, "RFI"],
+            cv=data[i, "RFI.CV"],
+            quality=data[i, "Class."]
+        )
+            
+        df <- rbind(df, tmpDf)
+    }
+    
+    return(df)
+}
+
+tmp <- readZeptosensExport(inputFile="inst/data/R006_RFI_Export_Table.xls",
+                    outputFile="out.txt",
+                    sampleNameEntries=c("sampleNameNumber", "NA", "NA", "sampleName", "treatment", "time", "replicate"))
+
+#' Read Intermediate Export File 
+#' 
+#' @param nrep number of replicates
+#' @param ncond number of conditions
+#' @param ntime number of timepoints 
+#' @param nab number of antibodies 
+#' 
+#' @expamples 
+#' #TODO
+#' 
+#' @concept zeptosensPkg
+#' @export 
+readIntermediateExport <- function(inputFile=NULL, outputFile=NULL, nrep=NULL, ncond=NULL, ntime=NULL, nab=NULL) {
     options("digits"=3)
     
     nrep=2
@@ -26,9 +82,9 @@ readZeptosensExport <- function(nrep=NULL, ncond=NULL, ntime=NULL, nab=NULL, out
     normal <- read.table("cellmix.txt")
     
     for (i in 1:length(data[,1])) {
-        if(data[i,5] == 'Poor') { 
-            data[i,3] = 0
-            data[i,4] = 0
+        if(data[i, 5] == 'Poor') { 
+            data[i, 3] = 0
+            data[i, 4] = 0
         }
     }
     
@@ -52,9 +108,7 @@ readZeptosensExport <- function(nrep=NULL, ncond=NULL, ntime=NULL, nab=NULL, out
         }
     }
     
-    write.table(datar, file="a2058_replicates_nn.txt", col.names=F, row.names=F, sep = "	",quote=FALSE)
-    g_datar=datar[which(as.numeric(datar[,6]) > 0 & as.numeric(datar[,7]) > 0 ),]
-    cc <- cor(as.numeric(g_datar[,6]),as.numeric(g_datar[,7]))
+    write.table(datar, file=outputFile, col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE)
 }
 
 #' Plot Zeptosens Replicates 
@@ -62,6 +116,9 @@ readZeptosensExport <- function(nrep=NULL, ncond=NULL, ntime=NULL, nab=NULL, out
 #' @param outputFile 
 #' @param 
 plotZeptosensReplicates <- function(data, outputFile="replicates.pdf", ) {
+    g_datar=datar[which(as.numeric(datar[,6]) > 0 & as.numeric(datar[,7]) > 0 ),]
+    cc <- cor(as.numeric(g_datar[,6]),as.numeric(g_datar[,7]))
+    
     pdf("replicates.pdf")
     plot(g_datar[,6],g_datar[,7],xlab="replicate 1",ylab="replicate 2")
     title(main=paste("a2058 replicates \nCC=",cc))
