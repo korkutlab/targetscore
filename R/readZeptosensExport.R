ff02251694f9e2f5c9a98882fdfe3d9c23e32f47
cannin <cannin@gmail.com>
2015-08-25 19:25:04 -0400

    Updated tmp.Rmd to calculate array ratios Changed RFI tables to Excel Added quick hack to Zeptosens reader

--- R/readZeptosensExport.R
+++ R/readZeptosensExport.R
@@ -23,19 +23,20 @@ readZeptosensExport <- function(inputFile=NULL, sampleNameEntries=NULL) {
     
     df <- NULL
     for(i in 1:nrow(data)) {
-        sampleNameNumberIdx <- which(sampleNameEntries == "sampleNameNumber")
+        #sampleNameNumberIdx <- which(sampleNameEntries == "sampleNameNumber")
         sampleNameIdx <- which(sampleNameEntries == "sampleName")
         treatmentIdx <- which(sampleNameEntries == "treatment")
         timeIdx <- which(sampleNameEntries == "time")
-        replicateIdx <- which(sampleNameEntries == "replicate")
+        #replicateIdx <- which(sampleNameEntries == "replicate")
         
+        # TODO: More generalized 
         tmpDf <- data.frame(
             sampleNumber=data[i, "Sample."],
-            sampleNameNumber=splitSampleNames[[i]][sampleNameNumberIdx],
+            #sampleNameNumber=splitSampleNames[[i]][sampleNameNumberIdx],
             sampleName=splitSampleNames[[i]][sampleNameIdx],
             treatment=splitSampleNames[[i]][treatmentIdx],
             time=splitSampleNames[[i]][timeIdx],
-            replicate=splitSampleNames[[i]][replicateIdx],
+            #replicate=splitSampleNames[[i]][replicateIdx],
             antibody=data[i, "Analyte"],
             readout=data[i, "RFI"],
             cv=data[i, "RFI.CV"],
@@ -47,3 +48,79 @@ readZeptosensExport <- function(inputFile=NULL, sampleNameEntries=NULL) {
     
     return(df)
 }
+
+# #' Read Intermediate Export File 
+# #' 
+# #' @param nrep number of replicates
+# #' @param ncond number of conditions
+# #' @param ntime number of timepoints 
+# #' @param nab number of antibodies 
+# #' 
+# #' @expamples 
+# #' #TODO
+# #' 
+# #' @concept zeptosensPkg
+# # readIntermediateExport <- function(data, outputFile=NULL, nrep=NULL, ncond=NULL, ntime=NULL, nab=NULL) {
+#     options("digits"=3)
+#     
+#     nrep=2
+#     ncond=3
+#     ntime=4
+#     nab=72
+#     
+#     ctr=ncond*ntime*nrep
+#     ct=ncond*ntime
+#     
+#     #data <- read.table("a2058.txt")
+#     annot <- read.table("ab_index.txt") antibodyName geneName postTranslationModification activityState
+# 
+#     normal <- read.table("cellmix.txt")
+#     
+#     for (i in 1:length(data[,1])) {
+#         if(data[i, 5] == 'Poor') { 
+#             data[i, 3] = 0
+#             data[i, 4] = 0
+#         }
+#     }
+#     
+#     data.matrix <- as.matrix(data)
+#     annot.matrix <- as.matrix(annot)
+#     datar <- array(dim=c(nab*ncond*ntime,7))
+#     
+#     for(ab in 1:nab){
+#         for (k in 1:ct) {
+#             #		print(k)
+#             datar[ct*(ab-1)+k,1]=data.matrix[ctr*(ab-1)+k,2]
+#             datar[ct*(ab-1)+k,2]=annot.matrix[ab,1]
+#             datar[ct*(ab-1)+k,3]=annot.matrix[ab,2]	
+#             datar[ct*(ab-1)+k,4]=annot.matrix[ab,3]	
+#             datar[ct*(ab-1)+k,5]=annot.matrix[ab,4]		
+#             #	datar[ct*(ab-1)+k,5]=data.matrix[ctr*(ab-1)+k,2]	
+#             #	datar[ct*(ab-1)+k,6]=data.matrix[ctr*(ab-1)+k,3]
+#             #	datar[ct*(ab-1)+k,7]=data.matrix[ctr*(ab-1)+ct+k,3]
+#             datar[ct*(ab-1)+k,6]=data[ctr*(ab-1)+k,4]#/(normal[ab,4])
+#             datar[ct*(ab-1)+k,7]=data[ctr*(ab-1)+ct+k,4]#/(normal[ab,4])
+#         }
+#     }
+#     
+#     write.table(datar, file=outputFile, col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE)
+# }
+# 
+# #' Plot Zeptosens Replicates 
+# #' 
+# #' @param outputFile 
+# #' @param 
+# plotZeptosensReplicates <- function(data, outputFile="replicates.pdf", ) {
+#     g_datar=datar[which(as.numeric(datar[,6]) > 0 & as.numeric(datar[,7]) > 0 ),]
+#     cc <- cor(as.numeric(g_datar[,6]),as.numeric(g_datar[,7]))
+#     
+#     pdf("replicates.pdf")
+#     plot(g_datar[,6],g_datar[,7],xlab="replicate 1",ylab="replicate 2")
+#     title(main=paste("a2058 replicates \nCC=",cc))
+#     dev.off()
+#     pdf("replicates_2.pdf")
+#     plot(g_datar[,6],g_datar[,7],xlab="replicate 1",ylab="replicate 2",xlim=c(0,2),ylim=c(0,2))
+#     title(main=paste("a2058 replicates\nCC=",cc))
+#     dev.off()
+#     rm(list=ls())
+# }
