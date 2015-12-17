3459f44a256c12457eae2ec671b114281f53823d
cannin <cannin@gmail.com>
2015-12-17 15:56:22 -0500

    Added check for invalid XLS Added paxtoolsr dependency

--- R/readZeptosensXls.R
+++ R/readZeptosensXls.R
@@ -13,7 +13,12 @@ readZeptosensXls <- function(inputFile) {
                     "numeric", "numeric", "numeric", "numeric", "numeric", "character", 
                     "character", "character", "numeric")
 
-    data <- read.xlsx2(inputFile, sheetIndex=1, colClasses=colClasses, stringsAsFactors=FALSE)
+    data <- tryCatch({
+        data <- read.xlsx2(inputFile, sheetIndex=1, colClasses=colClasses, stringsAsFactors=FALSE)
+    }, error = function(err) {
+        stop(paste("ERROR:  ", err, "\nTIP: The file may not be a valid XLS or XLSX file. Check that you cannot read the file in a text editor."))
+    })
+    
     rownames(data) <- 1:nrow(data)
     
     return(data)
