816ead5c1902f471309393aa275796c2cbafb03b
Korkut,Anil <AKorkut@mdanderson.org>
2016-10-19 16:54:41 -0500

    A new piece of code for  normalization of readouts over stdev of readouts of all samples. Preprocess the readouts using this code before calculating TS.

--- zeptosensPkg/R/sampSdev.R
+++ zeptosensPkg/R/sampSdev.R
@@ -1,22 +1,27 @@
-#standard deviation over all samples at each condition
-#' @param nX vertically merged data from all samples rows: condition columns: proteins
-#' @param sSdev standard deviation of response over all samples for each protein in each condition 
+#' stdev over all samples
+#' @param nX vertically merged data from all samples rows condition columns proteins
 #' @param nSample number of samples
 #' @param nProt number of proteins
 #' @param nCond number of consitions
+#' @concept zeptosensPkg
+#' @export
+#' 
+sampSdev <- function(nSample,nProt,nDose,nX){
+    
 
-
-sampSdev <- function(nX,sSdev,nSample,nProt,nCond)
     
 #    nProt
 #    nSample
 #    nCondition        
-
-    for (j in nProt){
+sSdev <- array(0:0, dim=c(nDose,nProt))
+for (i in 1:nSample){
+    
+   for (j in 1:nProt){
         
-        for (i in 1:nSample){
-            for (k in 1:nCond){
-                sSdev[i,j] <- sd(nX[((i-1)*nCond+1):i*nCond,j])
+            for (k in 1:nDose){
+                sSdev[k,j] <- sd(nX[((i-1)*nDose+1):i*nDose,j])
             }
     }
+    }
+return(sSdev)
 }
