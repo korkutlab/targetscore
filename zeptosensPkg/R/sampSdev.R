3969d0d2c30c2c0d4242818c20e72add33408c87
Korkut,Anil <AKorkut@mdanderson.org>
2016-10-21 17:25:54 -0500

    TS updates w/ stdev normalization & ovcar/jq1 TS calculations

--- zeptosensPkg/R/sampSdev.R
+++ zeptosensPkg/R/sampSdev.R
@@ -13,15 +13,25 @@ sampSdev <- function(nSample,nProt,nDose,nX){
 #    nProt
 #    nSample
 #    nCondition        
-sSdev <- array(0:0, dim=c(nDose,nProt))
-for (i in 1:nSample){
+    sSdev <- array(0:0, dim=c(nDose,nProt))
+    sSdev2 <- array(0:0, dim=c(nProt))
     
-   for (j in 1:nProt){
+#    for (i in 1:nSample){
         
+        for (j in 1:nProt){
+            
             for (k in 1:nDose){
-                sSdev[k,j] <- sd(nX[((i-1)*nDose+1):i*nDose,j])
+                sSdev[k,j] <- sd(nX[((k-1)*nSample+1):(k*nSample),j])
             }
-    }
-    }
-return(sSdev)
+        }
+#    }
+#    for (i in 1:nSample*nDose){
+        
+        for (j in 1:nProt){
+            
+                sSdev2[j] <- sd(nX[1:(nSample*nDose),j])
+
+        }
+#    }    
+return(sSdev2)
 }
