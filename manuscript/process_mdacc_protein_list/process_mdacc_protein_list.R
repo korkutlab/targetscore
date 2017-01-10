5310ec5ece0c9174dfd2e7da66f285faf6f9cef2
cannin <cannin@gmail.com>
2017-01-10 13:22:21 -0500

    Added full table

--- manuscript/process_mdacc_protein_list/process_mdacc_protein_list.R
+++ manuscript/process_mdacc_protein_list/process_mdacc_protein_list.R
@@ -3,6 +3,7 @@ library(readxl)
 dat <- read_excel("./process_mdacc_protein_list/protein_lists.xlsx", sheet=1, col_names=FALSE)
 
 abIds <- NULL
+simplifyFlag <- FALSE
 
 for (i in 1:nrow(dat)) {
   for(j in 1:ncol(dat)) {
@@ -10,23 +11,29 @@ for (i in 1:nrow(dat)) {
       
       tmp <- dat[i,j]
       
-      # Remove suffixes
-      tmp <- gsub('\\(.*\\)', '', tmp)
-      tmp <- gsub('\\.txt', '', tmp)
-      tmp <- gsub('_GBL.*$', '', tmp) 
-      tmp <- gsub('_GBL.*$', '', tmp) 
-      tmp <- gsub('_R[Ss].*$', '', tmp) 
-      
-      tmp <- gsub('[\\.-]w?[RGM][\\.-][QVEC].*$', '', tmp) 
-      tmp <- gsub('\\.R$', '', tmp) 
-      
-      # To uppercase
-      tmp <- toupper(tmp)
-    
-      # Change dashes
-      tmp <- gsub('\\.', '_', tmp) 
-      tmp <- gsub('-', '_', tmp) 
-      
+      if(simplifyFlag) {
+        # Remove suffixes
+        tmp <- gsub('\\(.*\\)', '', tmp)
+        tmp <- gsub('\\.txt', '', tmp)
+        tmp <- gsub('_GBL.*$', '', tmp) 
+        tmp <- gsub('_GBL.*$', '', tmp) 
+        tmp <- gsub('_R[Ss].*$', '', tmp) 
+        
+        tmp <- gsub('[\\.-]w?[RGM][\\.-][QVEC].*$', '', tmp) 
+        tmp <- gsub('\\.R$', '', tmp) 
+        
+        # To uppercase
+        tmp <- toupper(tmp)
+        
+        # Change dashes
+        tmp <- gsub('\\.', '_', tmp) 
+        tmp <- gsub('-', '_', tmp) 
+        
+        # For comparison
+        tmp <- gsub('_', '', tmp) 
+        tmp <- gsub('_', '', tmp) 
+      }
+
       abIds <- c(abIds, tmp)
     }
   }
@@ -34,5 +41,18 @@ for (i in 1:nrow(dat)) {
 
 tmp <- sort(unique(abIds))
 
-write.table(tmp, "./process_mdacc_protein_list/unique_abs.txt", col.names = FALSE, row.names = FALSE, quote=FALSE)
+df <- data.frame("AntibodyLabel"=tmp,
+                 "Source"=rep("MDACC", length(tmp)),
+                 "NodeName"=rep("", length(tmp)),
+                 "Gene_Symbol"=rep("", length(tmp)),
+                 "Sites"=rep("", length(tmp)),
+                 "Effect"=rep("", length(tmp)))
+
+
+if(simplifyFlag) {
+  write.table(df, "./process_mdacc_protein_list/unique_abs.txt", col.names = FALSE, row.names = FALSE, quote=FALSE)  
+} else {
+  write.table(df, "./process_mdacc_protein_list/unique_abs_full.txt", col.names = FALSE, row.names = FALSE, quote=FALSE)
+}
+
 
