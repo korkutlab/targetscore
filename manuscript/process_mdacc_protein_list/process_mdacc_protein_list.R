0e4648612bed70d068e5d8ad3c911b055639d364
cannin <cannin@gmail.com>
2017-01-10 15:29:45 -0500

    Updated output for MDACC processing

--- manuscript/process_mdacc_protein_list/process_mdacc_protein_list.R
+++ manuscript/process_mdacc_protein_list/process_mdacc_protein_list.R
@@ -3,56 +3,61 @@ library(readxl)
 dat <- read_excel("./process_mdacc_protein_list/protein_lists.xlsx", sheet=1, col_names=FALSE)
 
 abIds <- NULL
-simplifyFlag <- FALSE
+simpleNames <- NULL 
+tmpDf <- NULL
 
 for (i in 1:nrow(dat)) {
   for(j in 1:ncol(dat)) {
     if(!is.na(dat[i,j])) {
       
       tmp <- dat[i,j]
+      abIds <- c(abIds, tmp)
       
-      if(simplifyFlag) {
-        # Remove suffixes
-        tmp <- gsub('\\(.*\\)', '', tmp)
-        tmp <- gsub('\\.txt', '', tmp)
-        tmp <- gsub('_GBL.*$', '', tmp) 
-        tmp <- gsub('_GBL.*$', '', tmp) 
-        tmp <- gsub('_R[Ss].*$', '', tmp) 
-        
-        tmp <- gsub('[\\.-]w?[RGM][\\.-][QVEC].*$', '', tmp) 
-        tmp <- gsub('\\.R$', '', tmp) 
-        
-        # To uppercase
-        tmp <- toupper(tmp)
-        
-        # Change dashes
-        tmp <- gsub('\\.', '_', tmp) 
-        tmp <- gsub('-', '_', tmp) 
-        
-        # For comparison
-        tmp <- gsub('_', '', tmp) 
-        tmp <- gsub('_', '', tmp) 
+      # Remove suffixes
+      tmp <- gsub('\\(.*\\)', '', tmp)
+      tmp <- gsub('\\.txt', '', tmp)
+      tmp <- gsub('_GBL.*$', '', tmp) 
+      tmp <- gsub('_GBL.*$', '', tmp) 
+      tmp <- gsub('_R[Ss].*$', '', tmp) 
+      
+      tmp <- gsub('[\\.-]w?[RGM][\\.-][QVEC].*$', '', tmp) 
+      tmp <- gsub('\\.R$', '', tmp) 
+      
+      # To uppercase
+      #tmp <- toupper(tmp)
+      
+      # Change dashes
+      tmp <- gsub('\\.', '_', tmp) 
+      tmp <- gsub('-', '_', tmp) 
+      
+      # For table
+      nodeName <- gsub('_', '', tmp) 
+      symbol <- sub("^(.*)_p.*$", "\\1", tmp)
+      symbol <- gsub('_', '', symbol) 
+      symbol <- toupper(symbol)
+      
+      sites <- ''
+      if(grepl("p[TYS].*$", tmp)) {
+        sites <- sub(".*(p[TYS].*)$", "\\1", tmp)
       }
 
-      abIds <- c(abIds, tmp)
+      tmpDf <- rbind(tmpDf, data.frame(AntibodyLabel=dat[i,j], 
+                                       Source='MDACC',
+                                       NodeName=nodeName, 
+                                       Gene_Symbol=symbol, 
+                                       Sites=sites, 
+                                       Effect='',
+                                       FunctionalScore='',
+                                       stringsAsFactors=FALSE)) 
     }
   }
 }
 
-tmp <- sort(unique(abIds))
+tmpDf <- unique(tmpDf)
+tmpDf <- tmpDf[with(tmpDf, order(AntibodyLabel)), ]
 
-df <- data.frame("AntibodyLabel"=tmp,
-                 "Source"=rep("MDACC", length(tmp)),
-                 "NodeName"=rep("", length(tmp)),
-                 "Gene_Symbol"=rep("", length(tmp)),
-                 "Sites"=rep("", length(tmp)),
-                 "Effect"=rep("", length(tmp)))
+#write.table(tmp, "./process_mdacc_protein_list/unique_abs.txt", col.names = FALSE, row.names = FALSE, quote=FALSE)
+write.table(tmpDf, "./process_mdacc_protein_list/unique_abs_full.txt", sep="\t", col.names = TRUE, row.names = FALSE, quote=FALSE)
 
-
-if(simplifyFlag) {
-  write.table(df, "./process_mdacc_protein_list/unique_abs.txt", col.names = FALSE, row.names = FALSE, quote=FALSE)  
-} else {
-  write.table(df, "./process_mdacc_protein_list/unique_abs_full.txt", col.names = FALSE, row.names = FALSE, quote=FALSE)
-}
 
 
