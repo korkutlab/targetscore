f7b66fa19b021f10c54153273c4f5c6c27d9044f
cannin <cannin@gmail.com>
2017-03-15 16:16:38 -0400

    Updated documentation Updated causality with new causalpath jar

--- zeptosensPkg/R/zeptosensPkg.R
+++ zeptosensPkg/R/zeptosensPkg.R
@@ -2,8 +2,8 @@
 
 #' @import rJava
 .onLoad <- function(lib, pkg) {
-    .jpackage(pkg, jars = c("causality.jar"))
-    
+    .jpackage(pkg, jars = c("causalpath.jar"))
+
     # Taken from xlsxjars packages What's your java version?  Need >= 1.5.0.
     jversion <- .jcall("java.lang.System", "S", "getProperty", "java.version")
     if (jversion < "1.8.0") {
@@ -12,16 +12,16 @@
 }
 
 #' Skip a test if on Bioconductor
-#' 
+#'
 #' Extension on testthat code
-#' 
+#'
 #' @concept paxtoolsr
 #' @export
 skip_on_bioc <- function() {
-    if (identical(Sys.getenv("NOT_BIOC"), "true")) 
+    if (identical(Sys.getenv("NOT_BIOC"), "true"))
         return()
-    
+
     message <- "On Bioconductor"
     cond <- structure(list(message = message), class = c("skip", "condition"))
     stop(cond)
-} 
+}
