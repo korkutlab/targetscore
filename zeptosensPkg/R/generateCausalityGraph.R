f18fb2285ee90b37eaed0747cd7dd68820503d2c
cannin <cannin@gmail.com>
2017-03-17 18:15:01 -0400

    Updated causalpath

--- zeptosensPkg/R/generateCausalityGraph.R
+++ zeptosensPkg/R/generateCausalityGraph.R
@@ -9,8 +9,12 @@
 #' @param valueColumn Name of the values column in the measurements file
 #' @param valueThreshold The value threshold to be considered as significant
 #' @param graphType Either 'compatible' or 'conflicting'
-#' @param siteMatchStrict boolean option to enforce matching a phosphorylation site in the network with
-#'   the annotation of antibody
+#' @param doSiteMatch option to enforce matching a phosphorylation site in the network with the annotation of antibody
+#' @param siteMatchProximityThreshold when site matching is on, this parameter sets the proxomity threshold for a
+#'   site number in the relation to match the site whose change is observed in the data (DEFAULT: 0)
+#' @param siteEffectProximityThreshold when the site effect is not known, we can approximate it with the known
+#'   effect of proximate sites. This parameter sets the proximity threshold for using the proximate sites for that 
+#'   prediction. (DEFAULT: 0)
 #' @param geneCentric A boolean option to produce a gene-centric (TRUE) or an antibody-centric (FALSE) graph
 #' @param outputFilePrefix If the user provides xxx, then xxx.sif and xxx.format are generated
 #' @param customNetworkDirectory The directory that the network will be downloaded and SignedPC
@@ -20,9 +24,9 @@
 #' @export
 generateCausalityGraph <- function(platformFile, idColumn = "NodeName", symbolsColumn = "Symbol", sitesColumn = "Sites", 
     effectColumn = "Effect", valuesFile, valueColumn = "Value", valueThreshold, graphType = "compatible", 
-    siteMatchStrict = TRUE, geneCentric = TRUE, outputFilePrefix, customNetworkDirectory = tempdir()) {
+    doSiteMatch = TRUE, siteMatchProximityThreshold = 0, siteEffectProximityThreshold = 0, geneCentric = TRUE, outputFilePrefix, customNetworkDirectory = tempdir()) {
     
-    command <- "generateRPPAGraphs"
+    command <- "generateCausalityGraph"
     commandJStr <- .jnew("java/lang/String", command)
     
     platformFileJStr <- .jnew("java/lang/String", platformFile)
@@ -36,9 +40,11 @@ generateCausalityGraph <- function(platformFile, idColumn = "NodeName", symbolsC
     outputFilePrefixJStr <- .jnew("java/lang/String", outputFilePrefix)
     customNetworkDirectoryJStr <- .jnew("java/lang/String", customNetworkDirectory)
     
-    .jcall("org/panda/causalpath/run/CausalityAnalysisSingleMethodInterface", "V", command, platformFileJStr, idColumnJStr, 
-        symbolsColumnJStr, sitesColumnJStr, effectColumnJStr, valuesFileJStr, valueColumnJStr, 
-        valueThreshold, graphTypeJStr, siteMatchStrict, geneCentric, outputFilePrefixJStr, customNetworkDirectoryJStr)
+    .jcall("org/panda/causalpath/run/CausalityAnalysisSingleMethodInterface", "V", command, 
+           platformFileJStr, idColumnJStr, symbolsColumnJStr, sitesColumnJStr, effectColumnJStr, 
+           valuesFileJStr, valueColumnJStr, valueThreshold, graphTypeJStr, doSiteMatch, 
+           as.integer(siteMatchProximityThreshold), as.integer(siteEffectProximityThreshold), 
+           geneCentric, outputFilePrefixJStr, customNetworkDirectoryJStr)
     .jcheck()
     
     sifFile <- paste0(outputFilePrefix, ".sif")
