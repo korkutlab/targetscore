1f128e8b325f79d7c75b48c98f82181e3e5c837d
Heping Wang <>
2019-11-06 14:33:38 -0600

    Update optimize param with user defined penalty param

--- zeptosensPkg/R/optimize_parameter_dat.R
+++ zeptosensPkg/R/optimize_parameter_dat.R
@@ -2,25 +2,33 @@
 #'
 #' @param data input proteomics dataset for network inference. Gene in coloumns and samples in row.
 #' With colnames as gene tags and rownames as sample tags.
+#' @param rho positive tuning parameter for elastic net penalty. Default at seq(0.01,1,length=100).
 #'
-#' @return Parameter of regulization decided lowest BIC. Including regularize parameter
-#' (L1 norm parameter) as rho.
+#' @return Parameter of regulization BIC error calculated.
+#' \item{rho}{penalty parameter.}
+#' \item{bic}{BIC error calculated for penalty parameters.}
 #'
 #' @importFrom glasso glasso
 #'
 #' @concept zeptosensPkg
 #' @export
-optimize_parameter_dat <- function(data) {
+optimize_parameter_dat <- function(data, rho = seq(0.01, 1, length = 100)) {
+
+  # Calculate covariance matrix
   covmatrix <- cov(data)
-  rho <- seq(0.01, 1, length = 100)
+
+  # Set initial value
   bic <- rho
   g_result <- NULL
   p_off_d <- NULL
-  for (i in 1:100) {
+
+  # Calculate BIC for grid-search
+  for (i in seq_len(length(rho))) {
     g_result <- glasso::glasso(covmatrix, rho[i])
     p_off_d <- sum(g_result$wi != 0 & col(covmatrix) < row(covmatrix))
     bic[i] <- -2 * (g_result$loglik) + p_off_d * log(nrow(data))
   }
+
   rho <- rho[which.min(bic)]
   parameters <- list(rho = rho, bic = bic)
   return(parameters)
